62ce3ab40db4880b5a955b1ea85e04716e70f456
Sumit Mukhija <sumitmukhija@hotmail.com>
2020-04-02 23:35:08 +0100

    Hooked up train timetable and the interface

--- src/app/common-components/timetable/timetable.component.ts
+++ src/app/common-components/timetable/timetable.component.ts
@@ -19,6 +19,7 @@ import {
   ReactiveFormsModule,
   FormControl
 } from "@angular/forms";
+import { ThrowStmt } from '@angular/compiler';
 
 @NgModule({
   declarations: [TimetableComponent],
@@ -31,6 +32,11 @@ import {
   styleUrls: ["./timetable.component.css"]
 })
 export class TimetableComponent implements OnInit {
+
+  targetStation = [];
+  targetInfo = [];
+  name: ''
+
   constructor(
     public dialogRef: MatDialogRef<TimetableComponent>,
     private formBuilder: FormBuilder,
@@ -38,7 +44,48 @@ export class TimetableComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    
+    if (this.data.type === "irishrailstop") {
+      this.handleIrishRailData();
+    }
   }
+
+  handleIrishRailData() {
+    let xmlString = JSON.parse(localStorage.getItem("irishrailstopTimetable"))[
+      "schedule"
+    ];
+    let parser = new DOMParser();
+    let parsed = parser.parseFromString(xmlString, "application/xml");
+    console.log(parsed);
+    this.name = this.data.name;
+    let allStations = parsed.getElementsByTagName("objStationData");
+    for (let i = 0; i < allStations.length; i++) {
+      let currentStation = allStations[i];
+      let stationName = currentStation.getElementsByTagName("Stationfullname");
+      if (
+        stationName[0].textContent.toLowerCase().trim() ===
+        this.data.name.toLowerCase().trim()
+      ) {
+        this.targetStation.push(currentStation);
+      }
+    }
+    this.extractInfoFromTargetStation();
+  }
+
+  extractInfoFromTargetStation() {
+    if (this.targetStation && this.targetStation.length > 0) {
+      this.targetInfo = this.targetStation.map(node => {
+        return {
+          origin: node.getElementsByTagName("Origin")[0].textContent,
+          originTime: node.getElementsByTagName("Origintime")[0].textContent,
+          dest: node.getElementsByTagName("Destination")[0].textContent,
+          destTime: node.getElementsByTagName("Destinationtime")[0].textContent,
+          dueIn: node.getElementsByTagName("Duein")[0].textContent,
+          late: node.getElementsByTagName("Late")[0].textContent,
+          image: node.getElementsByTagName("Traintype")[0].textContent.toLowerCase().trim() == 'dart' ? "../../../assets/images/dart.png" : "../../../assets/images/irail.png"
+        };
+      });
+    }
+  }
+
 }
 
