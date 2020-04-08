5f09d7e9b6173628b7daa6e944257b0c3374c8b6
ag6102 <aashima.goel91@gmail.com>
2020-04-08 03:10:35 +0100

    Endpoint changes

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,19 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpHeaders } from "@angular/common/http";
 import { HttpHelperService } from "./http-helper.service.js";
 
 import config from "../../assets/config/dev-config.json";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 export class NotificationService {
-
-  constructor(private httpClient: HttpHelperService) { }
+  constructor(private httpClient: HttpHelperService) {}
 
   sendNotification(bundle) {
-    let url = config.API_ENDPOINTS.DEV_SERVER_BASE + "notify";
+    let url = config.API_ENDPOINTS.BASE_URL + "/notify";
     return this.httpClient.post(url, JSON.parse(bundle));
   }
 }
